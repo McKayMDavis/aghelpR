f96429f3006c491127a40cee7c153a8484690c7d
McKayMDavis <dav13037@byui.edu>
2018-07-17 14:30:59 -0600

    Add files via upload

---
+++ download_all.R
@@ -0,0 +1,21 @@
+#' Download all data files from each project (in "Data" folder) in main OSF project.
+#'
+#' @param id OSF id. Defaults to main Ag project.
+#'
+#' @param path Path to download files. Defaults to working directory.
+#'
+#' @export download_all
+
+download_all <- function(id = "judwb", path = getwd()) {
+  links <- get_all_file_links(id)
+
+  j <- 1
+
+  for (i in links) {
+    httr::GET(i, config = get_config(TRUE),
+              httr::write_disk(path, overwrite = TRUE))
+
+    cat(j, "/", length(links), "\n")
+    j <- j + 1
+  }
+}
