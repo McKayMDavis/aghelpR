5cf1c43b57584e676d5411a9d4592e79e90926ec
McKayMDavis <dav13037@byui.edu>
2018-09-05 16:21:59 -0600

    Add files via upload

--- R/move_to_projects.R
+++ R/move_to_projects.R
@@ -3,20 +3,23 @@
 #' @param id OSF id (osf.io/XXXX) (just "XXXX") of the parent node (Agriculture Division main project component id)
 #'
 #' @param year The current year component or year component to which file moving is desired
+#'
+#' @param from The file number from which the user wishes to begin. Defaults to the first file in the data folder.
 #' @export move_to_projects
 
-move_to_projects <- function(id = "judwb", year) {
+move_to_projects <- function(id = "judwb", year, from = 1) {
+  cat("Setting up. Please wait (This could take a couple minutes).\n")
   # Get current year's component, then get the project components (children) dictionary of that component
-  all_child_components <- get_dictionary(id = id, type = "children")
+  all_child_components <- suppressMessages(get_dictionary(id = id, type = "children"))
   working_child_id <- all_child_components[[year]]
-  project_components <- get_dictionary(id = working_child_id, type = "children")
+  project_components <- suppressMessages(get_dictionary(id = working_child_id, type = "children"))
   # NEED A WAY TO GET DELETE LINKS TO PASS TO MOVE FILES
   # NEED TO get_nodes ON TO COMPONENT ID
   # Get dictionary of files for current year
   data_component_id <- all_child_components[["Data"]]
-  raw_data_component <- get_dictionary(id = data_component_id, type = "children")
+  raw_data_component <- suppressMessages(get_dictionary(id = data_component_id, type = "children"))
   raw_data_component_id <- raw_data_component[["Raw_Data"]]
-  files_dict <- get_dictionary(id = raw_data_component_id, type = "files")
+  files_dict <- suppressMessages(get_dictionary(id = raw_data_component_id, type = "files"))
   current_files <- vector(mode = "list")
   for (i in names(files_dict)) {
     if (stringr::str_detect(i, year)) {
@@ -24,28 +27,29 @@ move_to_projects <- function(id = "judwb", year) {
     }
   }
   len <- length(current_files)
-  cur <- 1
+  cur <- from
 
   # Move files
   for (j in names(project_components)) {
-    for (k in names(current_files)) {
-      if (stringr::str_detect(tolower(k),
+    for (k in c(from:length(current_files))) {
+      l <- names(current_files[k])
+      if (stringr::str_detect(tolower(l),
                               tolower(stringr::str_replace_all(j,
                                                                "[^[:alnum:]]",
                                                                "")))) {
-        cat(k, "in project", j, ":", "\n")
+        cat(l, "in project", j, ":", "\n")
         # HERE
         # Might need to issue GET requests to grab info on the files in the project components.
-        proj_current <- get_dictionary(project_components[[j]], type = "files")
-        for (l in names(proj_current)) {
-          if (l %in% c("Data", "Journal", "Pictures")) {
-            req <- httr::GET(proj_current[[l]], config = get_config(TRUE))
+        proj_current <- suppressMessages(get_dictionary(project_components[[j]], type = "files"))
+        for (m in names(proj_current)) {
+          if (m %in% c("Data", "Journal", "Pictures")) {
+            req <- httr::GET(proj_current[[m]], config = get_config(TRUE))
             res <- rjson::fromJSON(httr::content(req, 'text', encoding = "UTF-8"))
-            for (m in seq_along(res$data)) {
-              if (!is.null(res$data[[m]]$attributes$name)) {
-                if (k == res$data[[m]]$attributes$name) {
-                  httr::DELETE(res$data[[m]]$links$delete, config = get_config(TRUE))
-                  cat("FILE: ", k, " DELETED\n")
+            for (o in seq_along(res$data)) {
+              if (!is.null(res$data[[o]]$attributes$name)) {
+                if (l == res$data[[o]]$attributes$name) {
+                  httr::DELETE(res$data[[o]]$links$delete, config = get_config(TRUE))
+                  cat("FILE: ", l, " DELETED\n")
                 }
               }
             }
@@ -53,12 +57,12 @@ move_to_projects <- function(id = "judwb", year) {
         }
 
         folders_dict <- get_dictionary(id = project_components[[j]], type = "folders")
-        if (stringr::str_detect(k, ".csv")) {
-          move(files = current_files, folders = folders_dict, file_name = k, folder_name = "Data")
-        } else if (stringr::str_detect(k, ".txt")) {
-          move(files = current_files, folders = folders_dict, file_name = k, folder_name = "Journal")
+        if (stringr::str_detect(l, ".csv")) {
+          move(files = current_files, folders = folders_dict, file_name = l, folder_name = "Data")
+        } else if (stringr::str_detect(l, ".txt")) {
+          move(files = current_files, folders = folders_dict, file_name = l, folder_name = "Journal")
         } else {
-          move(files = current_files, folders = folders_dict, file_name = k, folder_name = "Pictures")
+          move(files = current_files, folders = folders_dict, file_name = l, folder_name = "Pictures")
         }
         cat(cur, "/", len, "\n")
         cur <- cur + 1
